c70b7abdbf27164eae653747e1b4cec2f96f9a32
adriens <adrien.sales@gmail.com>
2018-03-29 18:14:57 +1100

    First swagger integration.

--- src/main/java/com/github/adriens/eaux/baignade/EauxBaignadesApplication.java
+++ src/main/java/com/github/adriens/eaux/baignade/EauxBaignadesApplication.java
@@ -3,9 +3,17 @@ package com.github.adriens.eaux.baignade;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableSwagger2
+@ComponentScan(basePackages = {"com.github.adriens.eaux.baignade"})
+@EntityScan("com.github.adriens.eaux.baignade.domain")
 public class EauxBaignadesApplication {
 
 	public static void main(String[] args) {
