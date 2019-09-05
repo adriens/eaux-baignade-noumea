24c49c6c4986702230e9825da9ee6dbc4de3f08e
SALES <Adrien.Sales@gmail.com>
2019-09-05 12:01:46 +1100

    Disable CORS security
    CORS issue : enable cross domain origins requests #37

--- src/main/java/com/github/adriens/eaux/baignade/controllers/PlagesController.java
+++ src/main/java/com/github/adriens/eaux/baignade/controllers/PlagesController.java
@@ -40,11 +40,13 @@ public class PlagesController {
     PlagesService plagesService;
 
     @RequestMapping("/")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public String hello() {
         return "Qualité des eaux de baignade tels que fournies sur http://www.noumea.nc/actualites/qualite-des-eaux-de-baignade-0";
     }
 
     @RequestMapping("/plages")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public List<PlageDetails> getAllPlages() throws Exception {
         try {
             return plagesService.getPlagesDetails();
@@ -55,6 +57,7 @@ public class PlagesController {
     }
 
     @RequestMapping("/plages/{plageId}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public PlageDetails getPlageDetails(
             @PathVariable(value = "plageId") int plageId) throws Exception {
         try {
@@ -65,23 +68,27 @@ public class PlagesController {
     }
 
     @RequestMapping("/drapeaux")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public List<Drapeau> getDrapeaux() {
         return plagesService.getDrapeaux();
     }
 
     @RequestMapping("/drapeaux/{drapeauId}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public Drapeau getDrapeau(
             @PathVariable(value = "drapeauId") String drapeauId) throws Exception {
         return plagesService.getDrapeau(drapeauId);
     }
 
     @RequestMapping("/metadatas")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public PageMetaDatas getPageMetaDatas() throws Exception {
         return plagesService.getPageMetaDatas();
     }
 
     // svg badges
     @RequestMapping(value = "/plages/{plageId}/badge.svg", method = RequestMethod.GET)
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Object> redirectToExternalUrl(
             @PathVariable(value = "plageId") int plageId) throws Exception {
         // get datas of the plage with id {id}
@@ -103,6 +110,7 @@ public class PlagesController {
     
     // SHields dedicated endpoint
     @RequestMapping("/plages/{plageId}/shield")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ShieldEndpointResponse getShieldResponse(
             @PathVariable(value = "plageId") int plageId) throws Exception {
         try {
